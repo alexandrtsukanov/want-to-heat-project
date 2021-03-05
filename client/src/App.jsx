ed23079fb3a2cfa93c7d99878dc4dba5970cef64
Alexandrtsukanov <alex217tsukanov@mail.ru>
2021-03-05 18:35:38 +0300

    frame

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css';
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { checkUserSession } from '../src/redux/actions/actions';
 
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 import Login from './Components/Login';
 import Logout from './Components/Logout';
 import Register from './Components/Register';
 import Profile from './Components/Profile';
-
+import Search from './Components/Search';
 
 function App() {
 
@@ -25,12 +25,15 @@ function App() {
     <>
       <Router>
         <Navbar />
+        <Home />
         <Switch>
           <Route exact path='/'>
             {user._id ?
-              (<div>Content</div>)
-              :
-              (<h2>Please login or register first</h2>)
+              <Search /> :
+              <h2>Please login or register first</h2>
+              // (<div>Content</div>)
+              // :
+              // (<h2>Please login or register first</h2>)
             }
           </Route>
           <Route path='/logout'>
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
@@ -7,30 +7,74 @@ function Navbar() {
 
   return (
     <>
-      <div>
-        <Link className='a' to='/'>
+      {/* <div>
+        <Link classNameNameName='a' to='/'>
           Home
         </Link>
         {user._id ?
           (<>
-            <Link className='a' to={`/user/${user._id}`}>
+            <Link classNameNameName='a' to={`/user/${user._id}`}>
               Profile
             </Link>
-            <Link className='a' to='/logout'>
+            <Link classNameNameName='a' to='/logout'>
               Logout
             </Link>
           </>)
           :
           (<>
-            <Link className='a' to='/login'>
+            <Link classNameNameName='a' to='/login'>
               Login
            </Link>
-            <Link className='a' to='/register'>
+            <Link classNameNameName='a' to='/register'>
               Register
            </Link>
           </>)
         }
+      </div> */}
+
+<header id="header" className="fixed-top d-flex align-items-center  header-transparent ">
+    <div className="container d-flex align-items-center">
+
+      <div className="logo mr-auto">
+        <h1 className="text-light"><a href="index.html">I want to heat</a></h1>
+        {/* Uncomment below if you prefer to use an image logo
+        <a href="index.html"><img src="assets/img/logo.png" alt="" className="img-fluid"/></a> */}
       </div>
+
+      <nav className="nav-menu d-none d-lg-block">
+        <ul>
+          <li className="active"><a href="index.html">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Sign Up</a></li>
+          <li><a href="#portfolio">Log In</a></li>
+          <li><a href="#pricing">Profile</a></li>
+          <li><a href="#team">Team</a></li>
+          <li className="drop-down"><a href="">Log Out</a>
+            {/* <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li className="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul> */}
+          </li>
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+      </nav>
+
+    </div>
+  </header>
+
+
     </>
   )
 }
