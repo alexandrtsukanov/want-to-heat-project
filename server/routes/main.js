872f3b078bfd04318942e1be62dfce01ddb252d0
DzyubaAnton <bagor666@gmail.com>
2021-07-05 13:24:53 +0300

    fix links

--- server/routes/main.js
+++ server/routes/main.js
@@ -41,7 +41,7 @@ router.get('/google', passport.authenticate('google', {
 // hand control to passport to use code to grab profile info
 router.get('/google/redirect', passport.authenticate('google'),
   (req, res) => {
-    res.redirect('http://localhost:3000/filter');
+    res.redirect('https://teplo.herokuapp.com/filter');
   });
 
 // ==================register=======================
