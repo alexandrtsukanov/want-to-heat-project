201d558a056c99554dc15b607cb54b03bdaa5508
DzyubaAnton <bagor666@gmail.com>
2021-07-21 20:08:53 +0300

    added video

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -21,10 +21,10 @@ function Home () {
     <div id="heroCarousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
       <div className="carousel-item active">
         <div className="carousel-container">
-          <h2 style={{fontSize: '48px'}} className="animate__animated animate__fadeInDown">Хочу в тепло!</h2>
-          <p style={{fontSize: '24px'}} className="animate__animated animate__fadeInUp">"Хочу в тепло!" - это уникальный сервис, который позволяет найти и подобрать для Вас путёвки и авиабилеты, основываясь на температуре воздуха в желаемой Вами локации. С нашей помощью Вы сможете найти подходящий тур или авиабилет максимально быстро!</p>
+          <h2 style={{fontSize: '40px'}} className="animate__animated animate__fadeInDown">Хочу в тепло!</h2>
+          <p style={{fontSize: '20px'}} className="animate__animated animate__fadeInUp">"Хочу в тепло!" - это уникальный сервис, который позволяет найти и подобрать для Вас путёвки и авиабилеты, основываясь на температуре воздуха в желаемой Вами локации. С нашей помощью Вы сможете найти подходящий тур или авиабилет максимально быстро!</p>
           <div>
-
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/jzqCPPcDv8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           <span id="btn-get-started-left">
           <Link to="/register" style={{width: '5.6cm', textAlign: 'center'}} className="btn-register-home btn-get-started animate__animated animate__fadeInUp scrollto">Регистрация</Link>
           </span>
@@ -53,21 +53,6 @@ function Home () {
     </svg>
 
     </section>
-
-    // <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
-      // <defs>
-      //   <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
-      // </path></defs>
-      // <g class="wave1">
-      //   <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
-      // </use></g>
-      // <g class="wave2">
-      //   <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
-      // </use></g>
-      // <g class="wave3">
-      //   <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
-      // </use></g>
-    // </svg>
 
   )
 }
