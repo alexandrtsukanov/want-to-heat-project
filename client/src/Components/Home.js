d41d1708cccaec231468890b965fd84f1dedb5ff
DzyubaAnton <bagor666@gmail.com>
2021-07-21 20:24:30 +0300

    added video

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -3,7 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../redux/actions/userAction';
 
-function Home () {
+function Home() {
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -17,40 +17,40 @@ function Home () {
 
   return (
 
-<section id="hero" class="home d-flex flex-column justify-content-end align-items-center">
-    <div id="heroCarousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
-      <div className="carousel-item active">
-        <div className="carousel-container">
-          <h2 style={{fontSize: '40px'}} className="animate__animated animate__fadeInDown">Хочу в тепло!</h2>
-          <p style={{fontSize: '20px'}} className="animate__animated animate__fadeInUp">"Хочу в тепло!" - это уникальный сервис, который позволяет найти и подобрать для Вас путёвки и авиабилеты, основываясь на температуре воздуха в желаемой Вами локации. С нашей помощью Вы сможете найти подходящий тур или авиабилет максимально быстро!</p>
-          <div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/jzqCPPcDv8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <span id="btn-get-started-left">
-          <Link to="/register" style={{width: '5.6cm', textAlign: 'center'}} className="btn-register-home btn-get-started animate__animated animate__fadeInUp scrollto">Регистрация</Link>
-          </span>
-          <span id="btn-get-started-right">
-          <Link to="/login" className="btn-get-started animate__animated animate__fadeInUp scrollto">Войти</Link>
-          </span>
-          </div>
+    <section id="hero" class="home d-flex flex-column justify-content-end align-items-center">
+      <div id="heroCarousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
+        <div className="carousel-item active">
+          <div className="carousel-container">
+            <h2 style={{ fontSize: '30px' }} className="animate__animated animate__fadeInDown">Хочу в тепло!</h2>
+            <p style={{ fontSize: '20px' }} className="animate__animated animate__fadeInUp">"Хочу в тепло!" - это уникальный сервис, который позволяет найти и подобрать для Вас путёвки и авиабилеты, основываясь на температуре воздуха в желаемой Вами локации. С нашей помощью Вы сможете найти подходящий тур или авиабилет максимально быстро!</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/jzqCPPcDv8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div>
+              <span id="btn-get-started-left">
+                <Link to="/register" style={{ width: '5.6cm', textAlign: 'center' }} className="btn-register-home btn-get-started animate__animated animate__fadeInUp scrollto">Регистрация</Link>
+              </span>
+              <span id="btn-get-started-right">
+                <Link to="/login" className="btn-get-started animate__animated animate__fadeInUp scrollto">Войти</Link>
+              </span>
+            </div>
 
+          </div>
         </div>
       </div>
-    </div>
 
-    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
-      <defs>
-        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
-      </defs>
-      <g class="wave1">
-        <use href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)"/>
-      </g>
-      <g class="wave2">
-        <use href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)"/>
-      </g>
-      <g class="wave3">
-        <use href="#wave-path" x="50" y="9" fill="#f2f2e5"/>
-      </g>
-    </svg>
+      <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+        <defs>
+          <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
+        </defs>
+        <g class="wave1">
+          <use href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)" />
+        </g>
+        <g class="wave2">
+          <use href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)" />
+        </g>
+        <g class="wave3">
+          <use href="#wave-path" x="50" y="9" fill="#f2f2e5" />
+        </g>
+      </svg>
 
     </section>
 
